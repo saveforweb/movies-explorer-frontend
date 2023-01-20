ea5b5b7a9df7e68349a0b392beedaedd3b9a0d33
Anton Popov <anton@bbbro.ru>
2023-01-21 00:26:52 +0300

    #1 final fixes

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -13,7 +13,7 @@ function AboutMe() {
                     <p className="about-me__profile-description-text">
                         Я&nbsp;родился и&nbsp;живу в&nbsp;Саратове, закончил факультет экономики СГУ. У&nbsp;меня есть жена и&nbsp;дочь. Я&nbsp;люблю слушать музыку, а&nbsp;ещё увлекаюсь бегом. Недавно начал кодить. С&nbsp;2015 года работал в&nbsp;компании &laquo;СКБ Контур&raquo;. После того, как прошёл курс по&nbsp;веб-разработке, начал заниматься фриланс-заказами и&nbsp;ушёл с&nbsp;постоянной работы.
                     </p>
-                    <a href="/" className="about-me__profile-description-link">Github</a>
+                    <a href="https://github.com/saveforweb" target="_blank" rel="noreferrer" className="about-me__profile-description-link">Github</a>
                 </div>
                 <div className="about-me__profile-image">
                     <img src={profilePhoto} alt="Фотография студента" className="about-me__profile-image-photo" />
